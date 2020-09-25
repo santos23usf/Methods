f91067ce62bcd2733d03793708c7259dab87676d
Catherine Santos <santos23@usf.edu>
2020-09-25 02:11:40 -0400

    Write a Method that prints to the Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+    Author: Catherine Santos
+    Date: 09/24/2020
+    Comments: This C# Console application code demonstrates the use of 
+              methods after getting input from a user. 
+
+*/
+
+
+using System;
+using System.Runtime.CompilerServices;
 
 namespace Methods
 {
@@ -6,7 +16,19 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            // Call Write method to display question asking user for input
+            Console.Write("Please enter your first and last name: ");
+
+            // This variable will gather the user's full name and used to display in greeting message
+            string fullName = Console.ReadLine();
+
+            // Call WriteLine method to display the greeting message with the user's name
+            Console.WriteLine("Hello " + fullName + " - Dream Big");
+
+            //Call WriteLine method to display instructions on how to exit program
+            Console.WriteLine("Press any key to exit the program....");
+            Console.ReadKey(true);
+            }
+
     }
 }
